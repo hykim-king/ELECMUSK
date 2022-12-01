4d7cfa1c7d816429991b5fe0c4d36547a44d865b
KimDongJu <rlaehdwn60@gmail.com>
2022-12-01 16:07:20 +0900

    동주 컨트롤러

---
+++ src/main/java/com/pcwk/ehr/login/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.pcwk.ehr.login.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("LoginController")
+@RequestMapping("login")
+public class LoginController {
+	
+	final Logger LOG = LogManager.getLogger(getClass());
+	
+	public LoginController() {}
+	
+	@RequestMapping(value="/login.do")
+	public String view() {
+		
+		return "user/login.jsp";
+	}
+	
+}
