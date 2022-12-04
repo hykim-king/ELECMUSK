4ee00e302191af28f3cd2c11092de483216e0cee
S.H.Heo <hsh928796@gmail.com>
2022-12-04 21:52:25 +0900

    2022.12.04

--- src/main/java/com/pcwk/ehr/station/api/StationApiController.java
+++ src/main/java/com/pcwk/ehr/station/api/StationApiController.java
@@ -30,7 +30,7 @@ public class StationApiController {
 			sb.append("?" + URLEncoder.encode("page", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));//PageNo
 			sb.append("&" + URLEncoder.encode("perPage", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));//PageSize
 			sb.append("&" + URLEncoder.encode("serviceKey", "UTF-8")//Service Key
-					+ "=m8mhLoQ0Q98g4kewmJc%2B3l2PboIiGXBoq8WLOkx1QNqRJzBDzbH2rCRhsBNwTFjbI77pAcduxM3M9%2FwrB%2BKS6Q%3D%3D");
+					+ "{YOUR SERVICE KEY}");
 			sb.append("&application/json;charset=UTF-8");//Content-Type
 			URL url = new URL(sb.toString());
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
