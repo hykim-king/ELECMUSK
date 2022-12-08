09569168a524cc60af4b96eb38245bfffbb2b853
C.I.Jeong <ckddls13@gmail.com>
2022-12-08 17:45:37 +0900

    evchart

---
+++ src/main/java/com/pcwk/ehr/chart/dao/evChartDaoImpl.java
@@ -0,0 +1,42 @@
+package com.pcwk.ehr.chart.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.pcwk.ehr.chart.domain.evChartVO;
+import com.pcwk.ehr.evcar.cmn.evDTO;
+import com.pcwk.ehr.evcar.cmn.evSearchVO;
+
+@Repository("evChart")
+public class evChartDaoImpl implements evChartDao {
+	final Logger LOG = LogManager.getLogger(getClass());
+	
+	final String NAMESPACE = "com.pcwk.ehr.evchart";
+	final String DOT = ".";
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	public evChartDaoImpl() {}
+
+	@Override
+	public List<evChartVO> doRetrieve(evDTO inVO) throws SQLException {
+		evSearchVO search = (evSearchVO) inVO;
+		List<evChartVO> list = new ArrayList<evChartVO>();
+		String statement = NAMESPACE+DOT+"doRetrieve";
+		list = sqlSessionTemplate.selectList(statement, search);
+		for(evChartVO vo : list) {
+			LOG.debug("│vo : "+ vo);
+		}
+		return list;
+	}
+
+
+}
