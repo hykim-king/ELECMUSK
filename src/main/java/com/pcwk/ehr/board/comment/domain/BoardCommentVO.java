810df325b8982b6bbf44e792007902ecf079fb0a
now_0o <dlwpdn0704@gmail.com>
2022-12-28 12:00:42 +0900

    d

--- src/main/java/com/pcwk/ehr/board/comment/domain/BoardCommentVO.java
+++ src/main/java/com/pcwk/ehr/board/comment/domain/BoardCommentVO.java
@@ -6,14 +6,15 @@ public class BoardCommentVO extends DTO {
 	private int cmSeq;// 댓글순번
 	private int bdSeq;// 게시판글 순번
 	private int regSeq; // 댓글 작성자 순번 (검색에 필요)
-	private int memberSeq;// 작성자  닉네임 
+	private int  memberSeq;// 작성자  닉네임 
+	private String  memberNickname;// 작성자  닉네임 
 	private String contents;// 댓글내용
 	private String regDt;// 등록일
 
 	public BoardCommentVO() {}
-	
 
-	public BoardCommentVO(int cmSeq, int bdSeq, int regSeq, int memberSeq, String contents, String regDt) {
+	public BoardCommentVO(int cmSeq, int bdSeq, int regSeq, int memberSeq, String contents,
+			String regDt) {
 		super();
 		this.cmSeq = cmSeq;
 		this.bdSeq = bdSeq;
@@ -23,73 +24,69 @@ public class BoardCommentVO extends DTO {
 		this.regDt = regDt;
 	}
 
-
 	public int getCmSeq() {
 		return cmSeq;
 	}
-
 
 	public void setCmSeq(int cmSeq) {
 		this.cmSeq = cmSeq;
 	}
 
-
 	public int getBdSeq() {
 		return bdSeq;
 	}
-
 
 	public void setBdSeq(int bdSeq) {
 		this.bdSeq = bdSeq;
 	}
 
-
 	public int getRegSeq() {
 		return regSeq;
 	}
-
 
 	public void setRegSeq(int regSeq) {
 		this.regSeq = regSeq;
 	}
 
-
 	public int getMemberSeq() {
 		return memberSeq;
 	}
-
 
 	public void setMemberSeq(int memberSeq) {
 		this.memberSeq = memberSeq;
 	}
 
+	public String getMemberNickname() {
+		return memberNickname;
+	}
+
+	public void setMemberNickname(String memberNickname) {
+		this.memberNickname = memberNickname;
+	}
 
 	public String getContents() {
 		return contents;
 	}
 
-
 	public void setContents(String contents) {
 		this.contents = contents;
 	}
-
 
 	public String getRegDt() {
 		return regDt;
 	}
 
-
 	public void setRegDt(String regDt) {
 		this.regDt = regDt;
 	}
 
-
 	@Override
 	public String toString() {
 		return "BoardCommentVO [cmSeq=" + cmSeq + ", bdSeq=" + bdSeq + ", regSeq=" + regSeq + ", memberSeq=" + memberSeq
-				+ ", contents=" + contents + ", regDt=" + regDt + ", toString()=" + super.toString() + "]";
+				+ ", memberNickname=" + memberNickname + ", contents=" + contents + ", regDt=" + regDt + ", toString()="
+				+ super.toString() + "]";
 	}
-
-
-
+	
+	
+	
 }
